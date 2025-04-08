45d24d46e999ec6f7177b518c21bdaced61798a5
strangem1n <strangem111n@gmail.com>
2025-04-08 09:11:31 +0900

    4월 1주차

--- 백준/Bronze/10769. 행복한지 슬픈지/행복한지 슬픈지.py
+++
@@ -1,9 +0,0 @@
-message = input()
-if message.count(":-)") == 0 and message.count(":-(") == 0:
-    print("none")
-elif message.count(":-)") == message.count(":-("):
-    print("unsure")
-elif message.count(":-)") > message.count(":-("):
-    print("happy")
-else:
-    print("sad")
